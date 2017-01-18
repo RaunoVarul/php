0259c68bda4da27a6f99831951b78b030288116d
Rauno <rauno.varul@khk.ee>
2017-01-18 15:06:19 +0200

    defining act. php

--- act.php
+++ act.php
@@ -8,6 +8,8 @@
 //get act element value from url
 $act = $http->get('act');
 //define act file path according to the element value
-$fn =
+$fn = ACTS_DIR.str_replace('.','/',$act).'.php';
+// output act file path
+echo $fn.'<br />';
 
 ?>
